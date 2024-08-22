669bbc7937a945665c7d2244cedea9a89b6a886a
BandiYamuna <144514644+BandiYamuna@users.noreply.github.com>
2024-08-22 11:00:52 +0530

    Update Test1.java

--- DemoProject/src/Lbrce/com/Test1.java
+++ DemoProject/src/Lbrce/com/Test1.java
@@ -3,6 +3,7 @@ package Lbrce.com;
 public class Test1 {
 	public static void main(String args[]) {
 		System.out.println("welcome to Devops");
+		System.out.println("This is my first lab");
 	}
 
 }
